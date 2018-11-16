e8d798b1c0857f32672bd1f597506e3480c60eec
chanM0 <mikec201722@gmail.com>
2018-11-15 21:03:42 -0800

    Initial Commit

---
+++ ArApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArApp
+//
+//  Created by Mike on 11/15/18.
+//  Copyright © 2018 DreamTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
